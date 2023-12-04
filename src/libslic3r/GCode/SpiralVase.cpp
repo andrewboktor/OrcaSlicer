f6db0b554d1c2fd5a423cd3443316de93adb49fa
Andrew Boktor <aboktor@microsoft.com>
2023-12-03 21:59:13 -0800

    Working perfectly, coulnd't ask for better

--- src/libslic3r/GCode/SpiralVase.cpp
+++ src/libslic3r/GCode/SpiralVase.cpp
@@ -25,6 +25,54 @@ long nearest(SpiralPoint p, std::vector<SpiralPoint> *population)
     return min_idx;
 }
 
+SpiralPoint subtract(SpiralPoint a, SpiralPoint b) {
+    return SpiralPoint(a.x-b.x, a.y-b.y);
+}
+
+SpiralPoint add(SpiralPoint a, SpiralPoint b) {
+    return SpiralPoint(a.x+b.x, a.y+b.y);
+}
+
+SpiralPoint scale(SpiralPoint a, float factor){
+    return SpiralPoint(a.x*factor, a.y*factor);
+}
+
+float dot(SpiralPoint a, SpiralPoint b) {
+    return a.x*b.x+a.y*b.y;
+}
+
+SpiralPoint nearest_point_on_line(SpiralPoint c, SpiralPoint a, SpiralPoint b, float& dist) {
+    SpiralPoint ab = subtract(b, a);
+    SpiralPoint ca = subtract(c, a);
+    float t = dot(ca, ab)/dot(ab,ab);
+    t=t>1?1:t;
+    t=t<0?0:t;
+    SpiralPoint closest= SpiralPoint(add(a, scale(ab, t)));
+    dist = distance(c, closest);
+    return closest;
+}
+
+SpiralPoint nearest_point_on_polygon(SpiralPoint p, std::vector<SpiralPoint>* polygon, bool& found, float& dist) {
+    if(polygon->size()<2) {
+        found=false;
+        return SpiralPoint(0,0);
+    }
+    float min     = std::numeric_limits<float>::max();
+    SpiralPoint closest(0,0);
+    for(unsigned long i=0; i<polygon->size()-1; i++) {
+        float currentDist=0;
+        SpiralPoint current = nearest_point_on_line(p, polygon->at(i), polygon->at(i+1), currentDist);
+        if(currentDist<min) {
+            min=currentDist;
+            closest=current;
+            found=true;
+        }
+    }
+    dist=min;
+    return closest;
+
+}
+
 std::string SpiralVase::process_layer(const std::string &gcode, bool last_layer)
 {
     /*  This post-processor relies on several assumptions:
@@ -82,9 +130,9 @@ std::string SpiralVase::process_layer(const std::string &gcode, bool last_layer)
     // Tapering the absolute extruder distances requires to process every extrusion value after the first transition
     // layer.
     bool  transition = m_transition_layer && m_config.use_relative_e_distances.value;
-    float layer_height_factor = layer_height / total_layer_length;
     float len = 0.f;
-    m_reader.parse_buffer(gcode, [&new_gcode, &z, total_layer_length, layer_height_factor, transition, &len, &current_layer, &previous_layer, &transition_gcode, &last_layer, &smooth_spiral, &max_xy_smoothing]
+    SpiralPoint last_point = previous_layer != NULL && previous_layer->size() >0? previous_layer->at(previous_layer->size()-1): SpiralPoint(0,0);
+    m_reader.parse_buffer(gcode, [&new_gcode, &z, total_layer_length, layer_height, transition, &len, &current_layer, &previous_layer, &transition_gcode, &last_layer, &smooth_spiral, &max_xy_smoothing, &last_point]
         (GCodeReader &reader, GCodeReader::GCodeLine line) {
         if (line.cmd_is("G1")) {
             if (line.has_z()) {
@@ -97,12 +145,12 @@ std::string SpiralVase::process_layer(const std::string &gcode, bool last_layer)
                 float dist_XY = line.dist_XY(reader);
                 if (dist_XY > 0) {
                     // horizontal move
-                    if (line.extruding(reader)) {
+                    if (line.has_e()) {
                         len += dist_XY;
                         float factor = len / total_layer_length;
-                        if (transition && line.has(E))
-                            // Transition layer, modulate the amount of extrusion from zero to the final value.
-                            line.set(reader, E, line.value(E) * factor);
+                        if (transition)
+                            // Transition layer, interpolate the amount of extrusion from zero to the final value.
+                            line.set(reader, E, line.e() * factor);
                         else if (last_layer) {
                             // We want the last layer to ramp down extrusion, but without changing z height!
                             // So clone the line before we mess with its Z and duplicate it into a new layer that ramps down E
@@ -111,21 +159,29 @@ std::string SpiralVase::process_layer(const std::string &gcode, bool last_layer)
                             transitionLine.set(reader, E, line.value(E) * (1 - factor));
                             transition_gcode += transitionLine.raw() + '\n';
                         }
-                        // This is the core of Spiral Vase mode, ramp up the Z smoothly
-                        line.set(reader, Z, z + len * layer_height_factor);
-                        if(smooth_spiral) {
+                        // This line is the core of Spiral Vase mode, ramp up the Z smoothly
+                        line.set(reader, Z, z + factor * layer_height);
+                        if (smooth_spiral) {
                             // Now we also need to try to interpolate X and Y
                             SpiralPoint p(line.x(), line.y()); // Get current x/y coordinates
                             current_layer->push_back(p);       // Store that point for later use on the next layer
+
                             if (previous_layer != NULL) {
-                                long nearest_index = nearest(p, previous_layer); // Find the nearest point on the previous layer
-                                if (nearest_index >= 0) {
-                                    SpiralPoint nearestp = previous_layer->at(nearest_index);
-                                    if (distance(p, nearestp) < max_xy_smoothing) {
-                                        // Interpolate between the point on this layer and the point on the previous layer
-                                        line.set(reader, X, factor * p.x + (1 - factor) * nearestp.x);
-                                        line.set(reader, Y, factor * p.y + (1 - factor) * nearestp.y);
-                                    }
+                                bool        found    = false;
+                                float       dist     = 0;
+                                SpiralPoint nearestp = nearest_point_on_polygon(p, previous_layer, found, dist);
+                                if (found && dist < max_xy_smoothing) {
+                                    SpiralPoint target = add(scale(nearestp, 1 - factor), scale(p, factor));
+                                    // Interpolate between the point on this layer and the point on the previous layer
+                                    line.set(reader, X, target.x);
+                                    line.set(reader, Y, target.y);
+                                    // We need to figure out the distance of this new line!
+                                    float modified_dist_XY = distance(last_point, target);
+                                    line.set(reader, E,
+                                             line.e() * modified_dist_XY / dist_XY); // Scale the extrusion amount according to change in length
+                                    last_point = target;
+                                } else {
+                                    last_point = p;
                                 }
                             }
                         }
